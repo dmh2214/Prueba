edecfc4f65fdbfa18d9b7635f36fab2b1b0312b8
root <root@DESKTOP-JD3FOVC.localdomain>
2018-11-17 14:58:29 -0500

    Adding Python file

---
+++ dul_sol3_hwk1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 22:08:04 2018
+
+@author: duldu
+"""
+
+
+#### Question 3 solution ####
+def sentence(s):
+    ss=s.split(' ') #split words into a list
+    m=sorted(ss, key=len) #sort list by word length
+    mylist =[(m[i], len(m[i])) for i in range(0,len(m))] 
+    print(mylist)
+# Define the variable s as a string variable and run function sentence() to get solution     
+s=('Mexico is a beautiful country') 
+sentence(s)
